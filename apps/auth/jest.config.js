2e0fd92a5ab66a3fd4fdde8868aecb89c5ff2880
Chen Junda <ddadaal@outlook.com>
2022-05-10 20:36:19 +0800

    auth: add test

---
+++ apps/auth/jest.config.js
@@ -0,0 +1,23 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require("ts-jest");
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require("./tsconfig");
+
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "env/.env.test" });
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  rootDir: ".",
+  preset: "ts-jest",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+  testMatch: [
+    "<rootDir>/tests/**/*.test.ts?(x)",
+  ],
+  coverageDirectory: `coverage/${process.env.NODE_ENV}`,
+  testTimeout: 30000,
+  coverageReporters: ["lcov"],
+  setupFilesAfterEnv: ["jest-extended/all"],
+};
---
+++ apps/auth/tests/createUser.test.ts
@@ -0,0 +1,80 @@
+import { FastifyInstance } from "fastify";
+import { NoSuchObjectError } from "ldapjs";
+import { buildApp } from "src/app";
+import { findUser, useLdap } from "src/auth/ldap/helpers";
+import { config } from "src/config";
+
+let server: FastifyInstance;
+
+beforeEach(async () => {
+  server = await buildApp();
+
+  await server.ready();
+});
+
+afterEach(async () => {
+  await server.close();
+});
+
+it("creates user and group", async () => {
+
+  const user = {
+    mail: "test@test.com",
+    id: 10,
+    identityId: "123",
+    name: "name",
+    password: "12#",
+  };
+
+  const userDn = `${config.LDAP_ATTR_UID}=${user.identityId},${config.LDAP_ADD_USER_BASE}`;
+  const groupDn = `cn=${user.identityId},${config.LDAP_ADD_GROUP_BASE}`;
+
+  await useLdap(server.log)(async (client) => {
+
+    function removeEvenNotExist(dn: string) {
+      return new Promise<void>((res, rej) => {
+        client.del(dn, (err) => {
+          if (err) {
+            if (err instanceof NoSuchObjectError) {
+              console.log("No entity with dn " + userDn);
+            } else {
+              rej(err);
+            }
+          }
+          res();
+        });
+      });
+    }
+
+    const removeUser = async () => {
+      await removeEvenNotExist(userDn);
+      await removeEvenNotExist(groupDn);
+    };
+
+    // remove the user if exists
+    await removeUser();
+
+    try {
+
+      const resp = await server.inject({
+        method: "POST",
+        url: "/user",
+        payload: user,
+      });
+
+      expect(resp.statusCode).toBe(204);
+
+      const ldapUser = await findUser(server.log, client, user.identityId);
+      expect(ldapUser).toBeDefined();
+
+      expect(ldapUser).toEqual({
+        dn: userDn,
+        identityId: user.identityId,
+        name: user.name,
+      });
+    } finally {
+      await removeUser();
+    }
+
+  });
+});
