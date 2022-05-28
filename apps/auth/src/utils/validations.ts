7161ac562730b31c8bc03fbe2ab53c25e6d7212f
Chen Junda <ddadaal@outlook.com>
2022-05-28 07:05:19 +0000

    fix: some ts error

--- apps/auth/src/utils/validations.ts
+++ apps/auth/src/utils/validations.ts
@@ -3,7 +3,7 @@ export type RequiredBy<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
 export function ensureNotUndefined<TObj, TKeys extends keyof TObj>(obj: TObj, keys: TKeys[]): RequiredBy<TObj, TKeys> {
   for (const key of keys) {
     if (obj[key] === undefined) {
-      throw new Error(`Field ${key} is undefined.`);
+      throw new Error(`Field ${String(key)} is undefined.`);
     }
   }
 
