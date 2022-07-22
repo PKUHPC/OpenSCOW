7712bb982891b583003611425ba063c668595483
Chen Junda <ddadaal@outlook.com>
2022-07-23 00:13:59 +0800

    fix: cluster ops wrong type

--- apps/portal-web/src/clusterops/slurm/index.ts
+++ apps/portal-web/src/clusterops/slurm/index.ts
@@ -1,5 +1,8 @@
+import { ClusterOps } from "src/clusterops/api";
 import { slurmAppOps } from "src/clusterops/slurm/app";
+import { slurmJobOps } from "src/clusterops/slurm/job";
 
-export const createSlurmClusterOps = (cluster: string) => ({
-  apps: slurmAppOps(cluster),
+export const createSlurmClusterOps = (cluster: string): ClusterOps => ({
+  app: slurmAppOps(cluster),
+  job: slurmJobOps(cluster),
 });
