a0b7f6a914b17ef72db9e7489befc3403008b2b2
Chen Junda <ddadaal@outlook.com>
2022-09-27 16:05:32 +0800

    fix(mis): redirect to dashboard by default

--- apps/mis-web/src/pages/index.tsx
+++ apps/mis-web/src/pages/index.tsx
@@ -1,25 +1,20 @@
 import { NextPage } from "next";
 import { useStore } from "simstate";
 import { Redirect } from "src/components/Redirect";
-import { PlatformRole } from "src/models/User";
 import { UserStore } from "src/stores/UserStore";
 
 export const IndexPage: NextPage = () => {
   const userStore = useStore(UserStore);
 
   if (userStore.user) {
-    if (userStore.user.platformRoles.length === 0 && userStore.user.accountAffiliations.length === 0) {
+    if (
+      userStore.user.platformRoles.length === 0 &&
+      userStore.user.tenantRoles.length === 0 &&
+      userStore.user.accountAffiliations.length === 0
+    ) {
       return <Redirect url="/noAccount" />;
     } else {
-      if (userStore.user.accountAffiliations.length > 0) {
-        return <Redirect url="/dashboard" />;
-      } else {
-        if (userStore.user.platformRoles.includes(PlatformRole.PLATFORM_FINANCE)) {
-          return <Redirect url="/finance/pay" />;
-        } else {
-          return <Redirect url="/dashboard"/>;
-        }
-      }
+      return <Redirect url="/dashboard" />;
     }
   } else {
     return <Redirect url="/api/auth" />;
