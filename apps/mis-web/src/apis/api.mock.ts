846d6928e2b16873e326084e5a8f39cf2bbd91da
qhqhqhq <quhan1130@gmail.com>
2022-10-09 17:28:04 +0800

    test(mis-web): add more test cases for getAllUsers API

--- apps/mis-web/src/apis/api.mock.ts
+++ apps/mis-web/src/apis/api.mock.ts
@@ -96,6 +96,12 @@ export const mockApi: MockApi<typeof api> = {
   getAllUsers: async () => ({ 
     totalCount: 3,
     platformUsers: [
+      {
+        userId: "123", 
+        name: "testuser", 
+        createTime: "2022-10-05T23:49:50.000Z", 
+        platformRoles: [PlatformRole.PLATFORM_FINANCE, PlatformRole.PLATFORM_ADMIN],
+      },
       { 
         userId: "test01", 
         name: "test01", 
