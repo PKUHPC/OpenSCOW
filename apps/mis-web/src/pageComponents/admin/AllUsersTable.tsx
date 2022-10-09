9521cec604f84ae6757028b5a9620390d553ef67
qhqhqhq <quhan1130@gmail.com>
2022-10-09 14:18:08 +0800

    feat(mis-web): write front-end page(without API)

--- apps/mis-web/src/pageComponents/admin/AllUsersTable.tsx
+++ apps/mis-web/src/pageComponents/admin/AllUsersTable.tsx
@@ -1,10 +1,13 @@
-import { Table, Tag } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { message, Modal, Space, Table } from "antd";
 import React, { useCallback, useState } from "react";
 import { useAsync } from "react-async";
 import { api } from "src/apis";
+import { DisabledA } from "src/components/DisabledA";
 import { PlatformUserInfo } from "src/generated/server/user";
-import { PlatformRoleTexts } from "src/models/User";
+import { PlatformRole } from "src/models/User";
 import { GetAllUsersSchema } from "src/pages/api/admin/getAllUsers";
+import { User } from "src/stores/UserStore";
 import { formatDateTime } from "src/utils/datetime";
 
 interface PageInfo {
@@ -14,9 +17,10 @@ interface PageInfo {
 
 interface Props {
   refreshToken: boolean;
+  user: User;
 }
 
-export const AllUsersTable: React.FC<Props> = ({ refreshToken }) => {
+export const AllUsersTable: React.FC<Props> = ({ refreshToken, user }) => {
 
   const [pageInfo, setPageInfo] = useState<PageInfo>({ page: 1, pageSize: 10 });
   
@@ -27,7 +31,7 @@ export const AllUsersTable: React.FC<Props> = ({ refreshToken }) => {
     } });
   }, [pageInfo]);
 
-  const { data, isLoading } = useAsync({ promiseFn, watch: refreshToken });
+  const { data, isLoading, reload } = useAsync({ promiseFn, watch: refreshToken });
 
   return (
     <div>
@@ -36,6 +40,8 @@ export const AllUsersTable: React.FC<Props> = ({ refreshToken }) => {
         pageInfo={pageInfo}
         setPageInfo={setPageInfo}
         isLoading={isLoading}
+        reload={reload}
+        user={user}
       />
     </div>
   );
@@ -46,10 +52,12 @@ interface UserInfoTableProps {
   pageInfo: PageInfo;
   setPageInfo?: (info: PageInfo) => void;
   isLoading: boolean;
+  reload: () => void;
+  user: User;
 }
 
 const UserInfoTable: React.FC<UserInfoTableProps> = ({
-  data, pageInfo, setPageInfo, isLoading,
+  data, pageInfo, setPageInfo, isLoading, reload, user,
 }) => {
   return (
     <>
@@ -71,8 +79,117 @@ const UserInfoTable: React.FC<UserInfoTableProps> = ({
         <Table.Column<PlatformUserInfo> dataIndex="createTime" title="创建时间" 
           render={(time: string) => formatDateTime(time)}
         />
-        <Table.Column<PlatformUserInfo> dataIndex="platformRoles" title="用户角色" 
-          render={(_, r) => r.platformRoles.map((x) => <Tag key={x}>{PlatformRoleTexts[x]}</Tag>)}
+        <Table.Column<PlatformUserInfo> dataIndex="platformRoles" title="平台管理员"
+          render={(_, r) => (
+            r.platformRoles.includes(PlatformRole.PLATFORM_ADMIN)
+              ? (
+                <Space size="middle">
+                  是
+                  <DisabledA
+                    disabled={r.userId === user.identityId}
+                    message="不能取消自己的平台管理员权限"
+                    onClick={() => {
+                      Modal.confirm({
+                        title: "确认取消管理员权限",
+                        icon: <ExclamationCircleOutlined />,
+                        content: `确认要移除用户${r.name}（ID：${r.userId}）的平台管理员权限？`,
+                        onOk: async () => {
+                          await api.unsetPlatformRole({
+                            // pass
+                          })
+                            .then(() => {
+                              message.success("操作成功！");
+                              reload();
+                            });
+                        },              
+                      });
+                    }}
+                  >
+                    取消
+                  </DisabledA>
+                </Space> 
+              ) : ( 
+                <Space size="middle">
+                  否
+                  <a
+                    onClick={() => {
+                      Modal.confirm({
+                        title: "确认设置为平台管理员",
+                        icon: <ExclamationCircleOutlined />,
+                        content: `确认要设置用户${r.name}（ID：${r.userId}）为平台管理员？`,
+                        onOk: async () => {
+                          await api.setPlatformRole({
+                            // pass
+                          })
+                            .then(() => {
+                              message.success("操作成功！");
+                              reload();
+                            });
+                        },              
+                      });
+                    }}
+                  >
+                    设置
+                  </a>
+                </Space>
+              )
+              
+          )}
+        />
+        <Table.Column<PlatformUserInfo> dataIndex="platformRoles" title="平台财务人员"
+          render={(_, r) => (
+            r.platformRoles.includes(PlatformRole.PLATFORM_FINANCE)
+              ? (
+                <Space size="middle">
+                  是
+                  <a
+                    onClick={() => {
+                      Modal.confirm({
+                        title: "确认取消财务人员权限",
+                        icon: <ExclamationCircleOutlined />,
+                        content: `确认要取消用户${r.name}（ID：${r.userId}）的财务人员权限？`,
+                        onOk: async () => {
+                          await api.unsetPlatformRole({
+                            // pass
+                          })
+                            .then(() => {
+                              message.success("操作成功！");
+                              reload();
+                            });
+                        },              
+                      });
+                    }}
+                  >
+                    取消
+                  </a>
+                </Space> 
+              ) : ( 
+                <Space size="middle">
+                  否
+                  <a
+                    onClick={() => {
+                      Modal.confirm({
+                        title: "确认设置为平台财务人员",
+                        icon: <ExclamationCircleOutlined />,
+                        content: `确认要设置用户${r.name}（ID：${r.userId}）为平台财务人员？`,
+                        onOk: async () => {
+                          await api.setPlatformRole({
+                            // pass
+                          })
+                            .then(() => {
+                              message.success("操作成功！");
+                              reload();
+                            });
+                        },              
+                      });
+                    }}
+                  >
+                    设置
+                  </a>
+                </Space>
+              )
+              
+          )}
         />
       </Table>
     </>
