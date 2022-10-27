f604b35accd61226640b201a13b94e0497436dae
Chen Junda <ddadaal@outlook.com>
2022-10-28 00:04:12 +0800

    fix(mis-server): mock global fetch

--- apps/mis-server/tests/admin/user.test.ts
+++ apps/mis-server/tests/admin/user.test.ts
@@ -1,3 +1,5 @@
+import "tests/data/fetch";
+
 import { asyncClientCall } from "@ddadaal/tsgrpc-client";
 import { Server } from "@ddadaal/tsgrpc-server";
 import { ChannelCredentials } from "@grpc/grpc-js";
@@ -11,7 +13,6 @@ import { PlatformRole, TenantRole, UserServiceClient } from "src/generated/serve
 import { reloadEntity } from "src/utils/orm";
 import { insertInitialData } from "tests/data/data";
 import { dropDatabase } from "tests/data/helpers";
-
 
 const anotherTenant = "anotherTenant";
 
