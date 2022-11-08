1e7dd946c25d2cb6c9b77a43a398aae2d8f5cafe
ljz <871005218@qq.com>
2022-11-08 10:50:38 +0800

    chore(mis/portal-web): modify the calling of ValidateToken from lib-auth

--- apps/portal-web/src/auth/token.ts
+++ apps/portal-web/src/auth/token.ts
@@ -1,16 +1,7 @@
-import { jsonFetch } from "@ddadaal/next-typed-api-routes-runtime/lib/client";
-import path from "path";
+import { validateToken as valToken } from "@scow/lib-auth"; 
 import { USE_MOCK } from "src/apis/useMock";
 import { UserInfo } from "src/models/User";
 import { runtimeConfig } from "src/utils/config";
-
-interface AuthValidateTokenSchema {
-  query: { token: string }
-  responses: {
-    200: UserInfo;
-    400: { code: "INVALID_TOKEN" };
-  }
-}
 
 export async function validateToken(token: string | undefined): Promise<UserInfo | undefined> {
 
@@ -23,11 +14,7 @@ export async function validateToken(token: string | undefined): Promise<UserInfo
 
   if (!token) { return undefined; }
 
-  const resp = await jsonFetch<AuthValidateTokenSchema>({
-    method: "GET",
-    path: path.join(runtimeConfig.AUTH_INTERNAL_URL, "/validateToken"),
-    query: { token },
-  }).catch(() => undefined);
+  const resp = await valToken(runtimeConfig.AUTH_INTERNAL_URL, token).catch(() => undefined);
 
   if (!resp) {
     return undefined;
