dc16096c5ba4ab6a13f730f75ef613a9b82649db
Chen Junda <ddadaal@outlook.com>
2022-11-25 14:37:08 +0800

    Revert "fix(portal-web): add init routes to initialize shell and websocket proxy server"
    This reverts commit 17c710a1d193461f61b79613937c75f188953573.

--- apps/portal-web/src/pages/api/proxy/[type]/[node]/[port]/[[...path]].ts
+++ apps/portal-web/src/pages/api/proxy/[type]/[node]/[port]/[[...path]].ts
@@ -10,57 +10,55 @@ export const config = {
   },
 };
 
-/**
- * Node的原生http服务器（http.Server）在收到WebSocket连接的时候将会触发一个`upgrade`事件，而且并不走正常的HTTP请求响应流程
- * 所以整个系统第一次启动后，在以HTTP形式访问此函数之前，到代理地址的WebSocket将会失败
- * 因为此时并没有注册upgrade事件的监听器，无法处理WebSocket连接。
- *
- * 所以系统启动后，需要手动触发一次到本地址的HTTP请求，以便注册upgrade事件的监听器
-  */
-export function registerWebSocketProxyServer(res: AugmentedNextApiResponse) {
-  if (res.socket.server.upgrade) { return false; }
-  // @ts-ignore
-  const server: http.Server = res.socket.server;
-
-  server.on("upgrade", async (req, socket, head) => {
-
-    const writeError = (statusLine: string, msg: string) => {
-      socket.end(`HTTP/1.1 ${statusLine}\r\n${msg}`);
-    };
-
-    const user = await checkCookie(() => true, req).catch(() => {
-      writeError("500 Internal Server Error", "Error when authenticating request");
-      return 500;
-    });
-
-    if (typeof user === "number") {
-      writeError("401 Unauthorized", "token is not valid");
-      return;
-    }
-
-    const url = req.url!;
-
-    const target = parseProxyTarget(url);
-
-    if (target instanceof Error) {
-      writeError("400 Bad Request", target.message);
-      return;
-    }
-
-    proxy.ws(req, socket, head, { target, ignorePath: true, xfwd: true }, (err) => {
-      console.error(err, "Error when proxing WS requests");
-      writeError("500 Internal Server Error", "Error when proxing WS requests");
-    });
-
-  });
-
-  res.socket.server.upgrade = true;
-
-  return true;
-}
-
 export default async (req: NextApiRequest, res: AugmentedNextApiResponse) => {
 
+  /**
+   * Node的原生http服务器（http.Server）在收到WebSocket连接的时候将会触发一个`upgrade`事件，而且并不走正常的HTTP请求响应流程
+   * 所以整个系统第一次启动后，在以HTTP形式访问此代理地址之前，到本地址的WebSocket将会失败
+   * 因为此时并没有注册upgrade事件的监听器，无法处理WebSocket连接。
+   *
+   * 所以系统启动后，需要手动触发一次到本地址的HTTP请求，以便注册upgrade事件的监听器
+   */
+  if (!res.socket.server.upgrade) {
+    // @ts-ignore
+    const server: http.Server = res.socket.server;
+
+    server.on("upgrade", async (req, socket, head) => {
+
+      const writeError = (statusLine: string, msg: string) => {
+        socket.end(`HTTP/1.1 ${statusLine}\r\n${msg}`);
+      };
+
+      const user = await checkCookie(() => true, req).catch(() => {
+        writeError("500 Internal Server Error", "Error when authenticating request");
+        return 500;
+      });
+
+      if (typeof user === "number") {
+        writeError("401 Unauthorized", "token is not valid");
+        return;
+      }
+
+      const url = req.url!;
+
+      const target = parseProxyTarget(url);
+
+      if (target instanceof Error) {
+        writeError("400 Bad Request", target.message);
+        return;
+      }
+
+      proxy.ws(req, socket, head, { target, ignorePath: true, xfwd: true }, (err) => {
+        console.error(err, "Error when proxing WS requests");
+        writeError("500 Internal Server Error", "Error when proxing WS requests");
+      });
+
+    });
+
+    res.socket.server.upgrade = true;
+    // reload the page at first call
+    return res.redirect(307, req.url!);
+  }
 
   const user = await checkCookie(() => true, req).catch(() => {
     res.status(500).send("Error when authenticating request");
@@ -90,8 +88,6 @@ export default async (req: NextApiRequest, res: AugmentedNextApiResponse) => {
       res.status(500).send(err);
     }
   });
-
-
 
 
 };
--- apps/portal-web/src/pages/api/shell/socketio.ts
+++ apps/portal-web/src/pages/api/shell/socketio.ts
@@ -15,85 +15,82 @@ export const config = {
   },
 };
 
-export function registerShellServer(res: AugmentedNextApiResponse) {
-  if (res.socket.server.io) { return false; }
-  // @ts-ignore
-  const io = new ServerIO(res.socket.server, {
-    path: join(process.env.NEXT_PUBLIC_BASE_PATH || "", "/api/shell/socketio"),
-  });
-
-  io.on("connection", async (socket) => {
-
-
-    const user = await checkCookie(() => true, socket.request);
-
-    if (typeof user === "number") {
-      console.log("[io] token is not valid");
-      socket.emit("data", "token is not valid");
-      socket.disconnect();
-      return;
-    }
-
-
-    const log = (message: string, ...optionalParams: any[]) => console.log(
-      `[io] [${user.identityId}] ${message}`, optionalParams);
-
-    log("Connection request received.");
-
-    const cluster = queryToString(socket.handshake.query.cluster);
-
-    if (!runtimeConfig.CLUSTERS_CONFIG[cluster]) {
-      throw new Error(`Unknown cluster ${cluster}`);
-    }
-
-    const path = queryToString(socket.handshake.query.path);
-
-    const { cols, rows } = socket.handshake.query;
-
-    const client = getClient(ShellServiceClient);
-
-    const stream = asyncDuplexStreamCall(client, "shell");
-
-    await stream.writeAsync({ message: { $case: "connect", connect: {
-      cluster, userId: user.identityId,
-      cols: queryToIntOrDefault(cols, 80),
-      rows: queryToIntOrDefault(rows, 30),
-      path,
-    } } });
-
-    log("Connected to shell");
-
-    stream.on("data", (chunk: ShellResponse) => {
-      switch (chunk.message?.$case) {
-      case "data":
-        socket.emit("data", chunk.message.data.data);
-        break;
-      case "exit":
-        socket.emit("exit", { exitCode: chunk.message.exit.code, signal: chunk.message.exit.signal });
-        break;
-      }
-    });
-
-    socket.on("resize", (data: { cols: number, rows: number }) => {
-      stream.write({ message: { $case: "resize", resize: { cols: data.cols, rows: data.rows } } });
-    });
-
-    socket.on("data", ([data]: [string]) => {
-      stream.write({ message:  { $case :"data", data: { data: Buffer.from(data) } } });
-    });
-
-    socket.on("disconnect", () => {
-      stream.write({ message: { $case: "disconnect", disconnect: {} } });
-      stream.end();
-    });
-
-  });
-
-  res.socket.server.io = io;
-
-  return true;
-}
-
 export default async (req: NextApiRequest, res: AugmentedNextApiResponse) => {
+
+  if (!res.socket.server.io) {
+    // @ts-ignore
+    const io = new ServerIO(req.socket.server, {
+      path: join(process.env.NEXT_PUBLIC_BASE_PATH || "", "/api/shell/socketio"),
+    });
+
+    io.on("connection", async (socket) => {
+
+
+      const user = await checkCookie(() => true, socket.request);
+
+      if (typeof user === "number") {
+        console.log("[io] token is not valid");
+        socket.emit("data", "token is not valid");
+        socket.disconnect();
+        return;
+      }
+
+
+      const log = (message: string, ...optionalParams: any[]) => console.log(
+        `[io] [${user.identityId}] ${message}`, optionalParams);
+
+      log("Connection request received.");
+
+      const cluster = queryToString(socket.handshake.query.cluster);
+
+      if (!runtimeConfig.CLUSTERS_CONFIG[cluster]) {
+        throw new Error(`Unknown cluster ${cluster}`);
+      }
+
+      const path = queryToString(socket.handshake.query.path);
+
+      const { cols, rows } = socket.handshake.query;
+
+      const client = getClient(ShellServiceClient);
+
+      const stream = asyncDuplexStreamCall(client, "shell");
+
+      await stream.writeAsync({ message: { $case: "connect", connect: {
+        cluster, userId: user.identityId,
+        cols: queryToIntOrDefault(cols, 80),
+        rows: queryToIntOrDefault(rows, 30),
+        path,
+      } } });
+
+      log("Connected to shell");
+
+      stream.on("data", (chunk: ShellResponse) => {
+        switch (chunk.message?.$case) {
+        case "data":
+          socket.emit("data", chunk.message.data.data);
+          break;
+        case "exit":
+          socket.emit("exit", { exitCode: chunk.message.exit.code, signal: chunk.message.exit.signal });
+          break;
+        }
+      });
+
+      socket.on("resize", (data: { cols: number, rows: number }) => {
+        stream.write({ message: { $case: "resize", resize: { cols: data.cols, rows: data.rows } } });
+      });
+
+      socket.on("data", ([data]: [string]) => {
+        stream.write({ message:  { $case :"data", data: { data: Buffer.from(data) } } });
+      });
+
+      socket.on("disconnect", () => {
+        stream.write({ message: { $case: "disconnect", disconnect: {} } });
+        stream.end();
+      });
+
+    });
+
+    res.socket.server.io = io;
+  }
   res.end();
 };
