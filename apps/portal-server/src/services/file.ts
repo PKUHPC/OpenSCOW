edf800ae34ef06db07b93d73ab5d3960ef618457
ljz <871005218@qq.com>
2022-11-28 14:12:27 +0800

    feat(portal-server): delete the error throwed of move

--- apps/portal-server/src/services/file.ts
+++ apps/portal-server/src/services/file.ts
@@ -138,7 +138,8 @@ export const fileServiceServer = plugin((server) => {
         const sftp = await ssh.requestSFTP();
         const error = await sftpRename(sftp)(fromPath, toPath).catch((e) => e);
         if (error) {
-          throw <ServiceError>{ code: status.INTERNAL, message: "rename failed", details: error };
+          // 无需throw，因为已经对重名文件做了检查
+          // throw <ServiceError>{ code: status.INTERNAL, message: "rename failed", details: error };
         }
 
         return [{}];
