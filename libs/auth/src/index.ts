e9cddbfedbc34119fcbe21c97003d35f2616a793
yhy <useremail@163.com>
2022-11-28 23:57:54 +0800

    fix(lib-auth): 将lib/auth/src下的getUser在index中进行export导出

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -2,4 +2,5 @@ export { changePassword } from "./changePassword";
 export { createUser } from "./createUser";
 export type { Capabilities } from "./getCapabilities";
 export { getCapabilities } from "./getCapabilities";
+export { getUser } from "./getUser";
 export { validateToken } from "./validateToken";
