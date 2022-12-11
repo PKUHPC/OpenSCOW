ca9656929d0a22584a20e5218e32dd207cf1723c
逍遥俊子 <huangjun0210-17@126.com>
2022-12-11 21:38:07 +0800

    fix(docs): 更新logo

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "SCOW",
+        title: "",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
