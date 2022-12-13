65637e9be53e8460adfea2f7f5a4490c4c6564ec
Chen Junda <ddadaal@outlook.com>
2022-12-13 10:51:01 +0800

    fix migration

--- apps/mis-server/src/migrations/Migration20221213020347.ts
+++ apps/mis-server/src/migrations/Migration20221213020347.ts
@@ -6,14 +6,14 @@ export class Migration20221213020347 extends Migration {
   async up(): Promise<void> {
     Object.entries(clusterIdMap).forEach(([prev, curr]) => {
       if (prev === curr) { return; }
-      this.addSql(`update \`job_info\` set \`cluster\` = \`${curr}\` where \`cluster\` = \`${prev}\`;`);
+      this.addSql(`update \`job_info\` set \`cluster\` = '${curr}' where \`cluster\` = '${prev}';`);
     });
   }
 
   async down(): Promise<void> {
     Object.entries(clusterIdMap).forEach(([prev, curr]) => {
       if (prev === curr) { return; }
-      this.addSql(`update \`job_info\` set \`cluster\` = \`${prev}\` where \`cluster\` = \`${curr}\`;`);
+      this.addSql(`update \`job_info\` set \`cluster\` = '${prev}' where \`cluster\` = '${curr}';`);
     });
   }
 
