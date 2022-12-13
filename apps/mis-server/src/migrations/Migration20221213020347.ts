3a4f05bab1fc2054ff70107f802b57ebe626e296
Chen Junda <ddadaal@outlook.com>
2022-12-13 10:21:18 +0800

    fix migration and ignore matched cluster names

--- apps/mis-server/src/migrations/Migration20221213020347.ts
+++ apps/mis-server/src/migrations/Migration20221213020347.ts
@@ -4,13 +4,15 @@ import { clusterIdMap } from "src/config/clusters";
 export class Migration20221213020347 extends Migration {
 
   async up(): Promise<void> {
-    Object.entries(clusterIdMap).forEach((prev, curr) => {
+    Object.entries(clusterIdMap).forEach(([prev, curr]) => {
+      if (prev === curr) { return; }
       this.addSql(`update \`job_info\` set \`cluster\` = \`${curr}\` where \`cluster\` = \`${prev}\`;`);
     });
   }
 
   async down(): Promise<void> {
-    Object.entries(clusterIdMap).forEach((prev, curr) => {
+    Object.entries(clusterIdMap).forEach(([prev, curr]) => {
+      if (prev === curr) { return; }
       this.addSql(`update \`job_info\` set \`cluster\` = \`${prev}\` where \`cluster\` = \`${curr}\`;`);
     });
   }
