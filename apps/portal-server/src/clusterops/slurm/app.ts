c038f573f512ec48f692d37a48fd9575cdce3ab3
WZD09 <2101210655@stu.pku.edu.cn>
2022-12-13 17:42:55 +0800

    fix running time undefined bug

--- apps/portal-server/src/clusterops/slurm/app.ts
+++ apps/portal-server/src/clusterops/slurm/app.ts
@@ -244,7 +244,7 @@ export const slurmAppOps = (cluster: string): AppOps => {
             state: runningJobInfo?.state ?? "ENDED",
             ready,
             dataPath: await sftpRealPath(sftp)(jobDir),
-            runningTime: runningJobInfo.runningTime,
+            runningTime: runningJobInfo.runningTime ?? "",
             timeLimit: runningJobInfo.timeLimit,
           });
 
