fc031303461713c620f525cc66aafc6526adec5f
逍遥俊子 <huangjun0210-17@126.com>
2022-12-14 10:27:03 +0800

    fix(docs): 修改首页title

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={``}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
