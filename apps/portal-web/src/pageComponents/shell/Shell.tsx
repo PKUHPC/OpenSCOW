2694787265a4789d3a95a7e0e436770975a08bf5
WZD09 <2101210655@stu.pku.edu.cn>
2022-12-28 15:20:16 +0800

    fix shell bug

--- apps/portal-web/src/pageComponents/shell/Shell.tsx
+++ apps/portal-web/src/pageComponents/shell/Shell.tsx
@@ -85,6 +85,7 @@ export const Shell: React.FC<Props> = ({ user, cluster, path }) => {
         });
 
         socket.on("data", (data: ArrayBuffer) => {
+          console.log("received data", typeof data, data, data.toString());
           term.write(Buffer.from(data));
         });
 
