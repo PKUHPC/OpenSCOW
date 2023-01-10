69d37948168657650869c5b25b93d3a4c35b7bcf
Chen Junda <ddadaal@outlook.com>
2023-01-10 09:24:46 +0800

    add login test

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -12,19 +12,18 @@
 
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("logins", async ({ page }) => {
+  await page.goto("http://localhost:5001/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // eslint-disable-next-line max-len
+  await expect(page).toHaveURL("http://localhost:5000/public/auth?callbackUrl=http%3A%2F%2Flocalhost%3A5001%2Fapi%2Fauth%2Fcallback");
+
+  await page.getByPlaceholder("用户名").click();
+  await page.getByPlaceholder("用户名").fill("demo_admin");
+  await page.getByPlaceholder("密码").fill("demo_admin");
+  await page.getByRole("button", { name: "登录" }).click();
+
+  await expect(page).toHaveURL("http://localhost:5001/dashboard");
+
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
