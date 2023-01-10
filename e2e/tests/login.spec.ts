b07cf00c09a33ae5ac12496c5f4a94b91df2c879
Chen Junda <ddadaal@outlook.com>
2023-01-10 09:48:22 +0800

    add an extra test

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -12,18 +12,33 @@
 
 import { expect, test } from "@playwright/test";
 
-test("logins", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5001/");
 
   // eslint-disable-next-line max-len
   await expect(page).toHaveURL("http://localhost:5000/public/auth?callbackUrl=http%3A%2F%2Flocalhost%3A5001%2Fapi%2Fauth%2Fcallback");
+});
 
-  await page.getByPlaceholder("用户名").click();
+test("logins with correct credentials", async ({ page }) => {
   await page.getByPlaceholder("用户名").fill("demo_admin");
   await page.getByPlaceholder("密码").fill("demo_admin");
   await page.getByRole("button", { name: "登录" }).click();
 
   await expect(page).toHaveURL("http://localhost:5001/dashboard");
+});
+
+test("errors out with wrong password", async ({ page }) => {
+  await page.getByPlaceholder("用户名").fill("demo_admin");
+  await page.getByPlaceholder("密码").fill("demo_admin1");
+  await page.getByRole("button", { name: "登录" }).click();
+
+  await expect(page.getByText("用户名/密码无效，请检查。")).toHaveCount(1);
+
+  // eslint-disable-next-line max-len
+  await expect(page).toHaveURL("http://localhost:5000/public/auth?callbackUrl=http%3A%2F%2Flocalhost%3A5001%2Fapi%2Fauth%2Fcallback");
 
 });
+
+
+
 
