dbf9def8b41b35d44b1142a17b112ba6f7d30140
Chen Junda <ddadaal@outlook.com>
2023-02-12 10:49:46 +0800

    fix(portal-web): 修复PublicClusterConfig的partition类型

--- apps/portal-web/src/utils/config.ts
+++ apps/portal-web/src/utils/config.ts
@@ -43,7 +43,7 @@ export interface ServerRuntimeConfig {
 
 export interface PublicClusterConfig {
   displayName: string;
-  slurm: { partitions: string[] }
+  slurm: { partitions: ClusterConfigSchema["slurm"]["partitions"] }
 }
 
 
