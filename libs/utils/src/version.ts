2b457f85af9432cc682a49d5126781c4eb4ef6db
Chen Junda <ddadaal@outlook.com>
2023-02-27 19:40:20 +0800

    impl

---
+++ libs/utils/src/version.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2022 Peking University and Peking University Institute for Computing and Digital Economy
+ * SCOW is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+import { readFileSync } from "fs";
+
+interface VersionJsonInfo {
+  tag?: string;
+  commit: string;
+}
+
+export interface VersionInfo extends VersionJsonInfo {
+  packageJsonVersion: string;
+}
+
+export function readVersionFile(filename = "version.json") {
+  const jsonInfo = JSON.parse(readFileSync(filename, "utf-8")) as VersionInfo;
+  const packageJson = JSON.parse(readFileSync("package.json", "utf-8"));
+
+  return {
+    ...jsonInfo,
+    packageJsonVersion: packageJson.version as string,
+  };
+}
+
+
