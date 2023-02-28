bdc990a0cc4c5c52fdfe21b1290919fac2eae026
Chen Junda <ddadaal@outlook.com>
2023-02-28 14:08:15 +0800

    feat: 启动容器时日志打印版本信息 (#477)
    在启动auth, portal-web, mis-web,
    portal-server和mis-server时在日志中打印版本信息，包含`tag`和`commit`。npm会打印package.json的信息
    
    
    ![image](https://user-images.githubusercontent.com/8363856/221754574-5b9b467a-aed4-438f-bdb1-7811636ae563.png)
    
    
    注意：PR构建的容器的所log出来的commit号不是PR的commit的commit号！参考：https://github.com/actions/checkout/issues/881
    
    实现：
    1. 在构建容器时，根据构建容器时的最新commit，生成一个`version.json`文件放在容器pwd下
    2. 在这些组件启动时读取version.json和package.json的内容并生成内容

---
+++ libs/utils/src/version.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2022 Peking University and Peking University Institute for Computing and Digital Economy
+ * SCOW is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+import { existsSync, readFileSync } from "fs";
+
+interface VersionJsonInfo {
+  tag?: string;
+  commit: string;
+}
+
+export interface VersionInfo extends VersionJsonInfo {}
+
+export function readVersionFile(versionJsonFileName = "version.json") {
+
+  const jsonInfo: VersionJsonInfo = existsSync(versionJsonFileName)
+    ? JSON.parse(readFileSync(versionJsonFileName, "utf-8"))
+    : {};
+
+  return jsonInfo; }
+
+
