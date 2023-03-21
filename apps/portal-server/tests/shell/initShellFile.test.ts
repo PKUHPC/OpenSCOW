07c812dd43abd75c714c2e85745b8012a518c394
ljz <871005218@qq.com>
2023-03-21 16:40:14 +0000

    fix a bug of getClusterLoginNode

--- apps/portal-server/tests/shell/initShellFile.test.ts
+++ apps/portal-server/tests/shell/initShellFile.test.ts
@@ -21,10 +21,10 @@ it("To test whether the scow-shell-file.sh is automatically copied", async () =>
   const cluster = "hpc01";
 
   await initShellFile(cluster, console);
-  const host = getClusterLoginNode(cluster);
-  if (!host) { throw new Error(`Cluster ${cluster} has no login node`); }
+  const address = getClusterLoginNode(cluster).address;
+  if (!address) { throw new Error(`Cluster ${cluster} has no login node`); }
 
-  await sshConnect(host, "test", rootKeyPair, console, async (ssh) => {
+  await sshConnect(address, "test", rootKeyPair, console, async (ssh) => {
     const sftp = await ssh.requestSFTP();
     const result = await sftpExists(sftp, SHELL_FILE_REMOTE);
     expect(result).toEqual(true);
