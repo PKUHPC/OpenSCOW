7c4c857f501fbad12d20f9aa599f19aa645457e6
Chen Junda <ddadaal@outlook.com>
2023-04-07 14:27:49 +0800

    fix(cli): 修复init出错的问题 (#549)
    之前cli的init无法使用，是因为`fs.promises.cp`复制目录在pkg环境下有问题。这个PR自己实现了复制目录的逻辑，修复了这个问题，同时增加了测试逻辑

---
+++ apps/cli/tests/utils.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2022 Peking University and Peking University Institute for Computing and Digital Economy
+ * SCOW is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+import { existsSync, promises as fsp } from "fs";
+import { join } from "path";
+
+export const configPath = "tests/init.test.ts";
+export const testBaseFolder = `tests/testFolder${process.env.JEST_WORKER_ID}`;
+
+export async function compareDirectories(dir1: string, dir2: string): Promise<boolean> {
+  const files1 = await fsp.readdir(dir1);
+  const files2 = await fsp.readdir(dir2);
+
+  if (files1.length !== files2.length) {
+    return false;
+  }
+
+  for (const file of files1) {
+    const filePath1 = join(dir1, file);
+    const filePath2 = join(dir2, file);
+
+    if (!existsSync(filePath2)) {
+      return false;
+    }
+
+    const stat1 = await fsp.stat(filePath1);
+    const stat2 = await fsp.stat(filePath2);
+
+    if (stat1.isDirectory() && stat2.isDirectory()) {
+      if (!(await compareDirectories(filePath1, filePath2))) {
+        return false;
+      }
+    } else if (stat1.isFile() && stat2.isFile()) {
+      const content1 = await fsp.readFile(filePath1);
+      const content2 = await fsp.readFile(filePath2);
+      if (content1.compare(content2) !== 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
