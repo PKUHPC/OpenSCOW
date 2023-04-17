05c7acf02d433a4e97ba111796dffd4b324cb40f
ZihanChen821 <827625357@qq.com>
2023-04-17 05:27:53 +0000

    fix(mis-web): 租户充值及余额展示小数位修复

--- apps/mis-web/src/pageComponents/admin/AllTenantsTable.tsx
+++ apps/mis-web/src/pageComponents/admin/AllTenantsTable.tsx
@@ -10,14 +10,16 @@
  * See the Mulan PSL v2 for more details.
  */
 
-import { moneyToNumber } from "@scow/lib-decimal";
 import { formatDateTime } from "@scow/lib-web/build/utils/datetime";
-import { GetTenantInfoResponse } from "@scow/protos/build/server/tenant";
+import { Money } from "@scow/protos/build/common/money";
+import { PlatformTenantsInfo } from "@scow/protos/build/server/tenant";
 import { Table } from "antd";
+import { ColumnsType } from "antd/es/table";
 import React, { useCallback } from "react";
 import { useAsync } from "react-async";
 import { api } from "src/apis";
 import { GetAllTenantsSchema } from "src/pages/api/admin/getAllTenants";
+import { moneyToString } from "src/utils/money";
 
 interface Props {
   refreshToken: boolean;
@@ -46,31 +48,49 @@ interface TenantInfoTableProps {
 const TenantInfoTable: React.FC<TenantInfoTableProps> = ({
   data, isLoading,
 }) => {
+
+  const columns: ColumnsType<PlatformTenantsInfo> = [
+    {
+      dataIndex: "tenantId",
+      title: "租户ID",
+    },
+    {
+      dataIndex: "tenantId",
+      title: "租户ID",
+    },
+    {
+      dataIndex: "tenantName",
+      title: "租户名称",
+    },
+    {
+      dataIndex: "userCount",
+      title: "用户数量",
+    },
+    {
+      dataIndex: "accountCount",
+      title: "账户数量",
+    },
+    {
+      dataIndex: "balance",
+      title: "余额",
+      render: (balance: Money | undefined) => moneyToString(balance || { positive: true, yuan: 0, decimalPlace: 0 }),
+
+    },
+    {
+      dataIndex: "createTime",
+      title: "创建时间",
+      render: (time: string | undefined) => formatDateTime(time || ""),
+    },
+  ];
+
   return (
-    <>
-      <Table
-        dataSource={data?.platformTenants}
-        loading={isLoading}
-        scroll={{ x: true }}
-      >
-        <Table.Column<GetTenantInfoResponse> dataIndex="tenantId" title="租户ID"></Table.Column>
-        <Table.Column<GetTenantInfoResponse> dataIndex="tenantName" title="租户名称" />
-        <Table.Column<GetTenantInfoResponse> dataIndex="userCount" title="用户数量" />
-        <Table.Column<GetTenantInfoResponse> dataIndex="accountCount" title="账户数量" />
-        <Table.Column<GetTenantInfoResponse>
-          dataIndex="balance"
-          title="余额"
-          render={(_, r) => {
-            return moneyToNumber(r.balance || { positive: true, yuan: 0, decimalPlace: 0 });
-          }}
-        />
-        <Table.Column<GetTenantInfoResponse>
-          dataIndex="createTime"
-          title="创建时间"
-          render={(time: string) => formatDateTime(time)}
-        />
-      </Table>
-    </>
+    <Table
+      dataSource={data?.platformTenants}
+      columns={columns}
+      loading={isLoading}
+      scroll={{ x: true }}
+      pagination={false}
+    />
   );
 };
 
