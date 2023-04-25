13884c038c2a73a1ab53d84b358459157fc0c418
Chen Junda <ddadaal@outlook.com>
2023-04-25 11:55:59 +0800

    add tests

--- libs/ssh/tests/sshKey.test.ts
+++ libs/ssh/tests/sshKey.test.ts
@@ -11,12 +11,12 @@
  */
 
 import { join } from "path";
-import { insertKeyAsRoot } from "src/key";
-import { sftpExists, sftpReadFile, sftpStat } from "src/sftp";
-import { insertKeyAsUser, sshConnect, sshRmrf } from "src/ssh";
+import { insertKeyAsRoot, insertKeyAsUser, KeyPair } from "src/key";
+import { sftpChmod, sftpChown, sftpExists, sftpMkdir, sftpReadFile, sftpStat, sftpWriteFile } from "src/sftp";
+import { sshConnect, sshRmrf } from "src/ssh";
 
 import { connectToTestServerAsRoot,
-  createTestItems, resetTestServerAsRoot, rootKeyPair, target, TestSshServer } from "./utils";
+  createTestItems, generateSshKeyPair, resetTestServerAsRoot, rootKeyPair, target, TestSshServer } from "./utils";
 
 let serverSsh: TestSshServer;
 const randomPostfix = String(Math.ceil(Math.random() * 1000 + 1));
@@ -29,7 +29,7 @@ const password = "12345678";
 beforeEach(async () => {
   serverSsh = await connectToTestServerAsRoot();
   await createTestItems(serverSsh);
-  // creat user
+  // create user
   await serverSsh.ssh.execCommand(`adduser -D -h ${home} ${testUser}`);
   await serverSsh.ssh.execCommand(`echo ${testUser}:${password}|chpasswd`);
 });
@@ -64,17 +64,20 @@ it("insert key as root", async () => {
   const userID = await serverSsh.ssh.execCommand(`id -u ${testUser}`);
   const userGID = await serverSsh.ssh.execCommand(`id -g ${testUser}`);
 
+  const uid = Number(userID.stdout.trim());
+  const gid = Number(userGID.stdout.trim());
+
   const keyStats = await sftpStat(serverSsh.sftp)(keyFile);
   const keyPermission = (keyStats.mode & parseInt("777", 8)).toString(8);
   expect(keyPermission).toEqual("644");
-  expect(keyStats.uid).toBe(Number(userID.stdout.trim()));
-  expect(keyStats.gid).toBe(Number(userGID.stdout.trim()));
+  expect(keyStats.uid).toBe(uid);
+  expect(keyStats.gid).toBe(gid);
 
   const sshStats = await sftpStat(serverSsh.sftp)(sshDir);
   const sshPermission = (sshStats.mode & parseInt("777", 8)).toString(8);
   expect(sshPermission).toEqual("700");
-  expect(sshStats.uid).toBe(Number(userID.stdout.trim()));
-  expect(sshStats.gid).toBe(Number(userGID.stdout.trim()));
+  expect(sshStats.uid).toBe(uid);
+  expect(sshStats.gid).toBe(gid);
 });
 
 it("insert keys as user", async () => {
@@ -86,4 +89,45 @@ it("insert keys as user", async () => {
   expect(await sftpExists(serverSsh.sftp, keyFile)).toBeTrue();
 
   await tryLoginAsUser();
+});
+
+
+describe("doesn't override authorized_keys if exists", () => {
+
+  let existingKey: KeyPair;
+
+  beforeEach(async () => {
+
+    existingKey = await generateSshKeyPair();
+
+    const userUID = Number((await serverSsh.ssh.execCommand(`id -g ${testUser}`)).stdout);
+    const userGID = Number((await serverSsh.ssh.execCommand(`id -u ${testUser}`)).stdout);
+
+    await sftpMkdir(serverSsh.sftp)(sshDir);
+    await sftpChown(serverSsh.sftp)(sshDir, userUID, userGID);
+    await sftpWriteFile(serverSsh.sftp)(keyFile, existingKey.publicKey);
+    await sftpChown(serverSsh.sftp)(keyFile, userUID, userGID);
+    await sftpChmod(serverSsh.sftp)(keyFile, "644");
+  });
+
+  const checkExistingKeys = async () => {
+    const keyContent = (await sftpReadFile(serverSsh.sftp)(keyFile)).toString();
+    expect(keyContent).toInclude(existingKey.publicKey);
+  };
+
+  it("as user", async () => {
+
+    await insertKeyAsUser(target, testUser, password, rootKeyPair, console);
+    await tryLoginAsUser();
+
+    await checkExistingKeys();
+  });
+
+  it("as root", async () => {
+
+    await insertKeyAsRoot(testUser, target, rootKeyPair, console);
+    await tryLoginAsUser();
+    await checkExistingKeys();
+  });
+
 });
