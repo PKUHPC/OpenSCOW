a2a54ca1e5708deec8e638a9fd9605553cf1bb0e
Chen Junda <ddadaal@outlook.com>
2023-04-26 12:18:43 +0800

    faet

--- scripts/tag.mjs
+++ scripts/tag.mjs
@@ -11,7 +11,7 @@
  */
 
 /**
- * Compare the version of the current package.json with the version of the last commit
+ * Compare the version of the current package.json and protos/package.json with the version of the last commit
  * If the version is changed, create a tag and push it to the remote repository
  */
 
@@ -20,20 +20,40 @@ import { readFileSync } from "fs";
 
 const exec = (cmd) => execSync(cmd, { stdio: "inherit", encoding: "utf-8" });
 
-const lastPackageJson = execSync("git --no-pager show HEAD^1:package.json", { encoding: "utf-8" });
+function getVersion(path) {
+  const lastFile = execSync(`git --no-pager show HEAD^1:${path}`, { encoding: "utf-8" });
 
-const lastVersion = JSON.parse(lastPackageJson).version;
+  const currentFile = readFileSync(path, { encoding: "utf-8" });
 
-const currentVersion = JSON.parse(readFileSync("package.json", { encoding: "utf-8" })).version;
+  const last = JSON.parse(lastFile).version;
 
-if (lastVersion === currentVersion) {
-  console.log("App version is not changed. Ignored");
-  process.exit(0);
+  const current = JSON.parse(currentFile).version;
+
+  return { last, current };
 }
 
-console.log("App version is changed from %s to %s", lastVersion, currentVersion);
+// Tag SCOW Release
+let changed = false;
+const rootVersion = getVersion("package.json");
 
-exec(`git tag -a v${currentVersion} -m 'Release v${currentVersion}'`);
-exec("git push --tags");
+if (rootVersion.current !== rootVersion.last) {
+  changed = true;
+  console.log("App version is changed from %s to %s", rootVersion.last, rootVersion.current);
+  exec(`git tag -a v${rootVersion.current} -m 'SCOW Release v${rootVersion.current}'`);
+} else {
+  console.log("App version is not changed. Ignored");
+}
 
+// Tag SCOW API Release
+const scowApiVersion = getVersion("protos/package.json");
 
+if (scowApiVersion.current !== scowApiVersion.last) {
+  changed = true;
+  console.log("SCOW API version is changed from %s to %s", scowApiVersion.last, scowApiVersion.current);
+  exec(`git tag -a api-v${scowApiVersion.current} -m 'SCOW API Release v${scowApiVersion.current}'`);
+}
+
+if (changed) {
+  console.log("New Tag Created. Push tags.");
+  exec("git push --tags");
+}
