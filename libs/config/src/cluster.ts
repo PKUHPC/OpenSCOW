bbc3074230d313c5eabb7bfe0e6013f9101f622d
qhqhqhq <quhan1130@gmail.com>
2023-05-09 07:59:07 +0000

    libs

--- libs/config/src/cluster.ts
+++ libs/config/src/cluster.ts
@@ -13,35 +13,37 @@
 import { GetConfigFn, getDirConfig } from "@scow/lib-config";
 import { Static, Type } from "@sinclair/typebox";
 import { DEFAULT_CONFIG_BASE_PATH } from "src/constants";
-import { SlurmMisConfigSchema } from "src/mis";
+// import { SlurmMisConfigSchema } from "src/mis";
 
 const CLUSTER_CONFIG_BASE_PATH = "clusters";
 
 export const ClusterConfigSchema = Type.Object({
   displayName: Type.String({ description: "集群的显示名称" }),
-  scheduler: Type.Enum({ slurm: "slurm" }, { description: "集群所使用的调度器，目前只支持slurm", default: "slurm" }),
-  slurm: Type.Object({
-    loginNodes: Type.Array(Type.String(), { description: "集群的登录节点地址", default: []}),
-    partitions: Type.Array(
-      Type.Object(
-        {
-          name: Type.String({ description: "分区名", pattern: "^[^ ]+$" }),
-          mem: Type.Integer({ description: "内存，单位M" }),
-          cores: Type.Integer({ description: "核心数" }),
-          gpus: Type.Integer({ description: "GPU卡数" }),
-          nodes: Type.Integer({ description: "节点数" }),
-          qos: Type.Optional(Type.Array(Type.String({ description: "QOS" }))),
-          comment: Type.Optional(Type.String({ description: "计费项说明" })),
-        },
-      ),
-      {
-        description: "分区信息，分区名、内存、核心数、GPU卡数、节点数、QOS、计费项说明",
-        default: [],
-      },
-    ),
-    mis: Type.Optional(SlurmMisConfigSchema),
-  }),
-  misIgnore: Type.Boolean({ description: "在实际进行MIS操作时忽略这个集群", default: false }),
+  adapterUrl: Type.String({ description: "调度器适配器服务地址" }),
+  loginNodes: Type.Array(Type.String(), { description: "集群的登录节点地址", default: []}),
+  // scheduler: Type.Enum({ slurm: "slurm" }, { description: "集群所使用的调度器，目前只支持slurm", default: "slurm" }),
+  // slurm: Type.Object({
+  //   loginNodes: Type.Array(Type.String(), { description: "集群的登录节点地址", default: []}),
+  //   partitions: Type.Array(
+  //     Type.Object(
+  //       {
+  //         name: Type.String({ description: "分区名", pattern: "^[^ ]+$" }),
+  //         mem: Type.Integer({ description: "内存，单位M" }),
+  //         cores: Type.Integer({ description: "核心数" }),
+  //         gpus: Type.Integer({ description: "GPU卡数" }),
+  //         nodes: Type.Integer({ description: "节点数" }),
+  //         qos: Type.Optional(Type.Array(Type.String({ description: "QOS" }))),
+  //         comment: Type.Optional(Type.String({ description: "计费项说明" })),
+  //       },
+  //     ),
+  //     {
+  //       description: "分区信息，分区名、内存、核心数、GPU卡数、节点数、QOS、计费项说明",
+  //       default: [],
+  //     },
+  //   ),
+  //   mis: Type.Optional(SlurmMisConfigSchema),
+  // }),
+  // misIgnore: Type.Boolean({ description: "在实际进行MIS操作时忽略这个集群", default: false }),
 });
 
 export type ClusterConfigSchema = Static<typeof ClusterConfigSchema>;
@@ -52,11 +54,11 @@ export const getClusterConfigs: GetConfigFn<Record<string, ClusterConfigSchema>>
   const config = getDirConfig(ClusterConfigSchema,
     CLUSTER_CONFIG_BASE_PATH, baseConfigPath ?? DEFAULT_CONFIG_BASE_PATH);
 
-  Object.entries(config).forEach(([id, c]) => {
-    if (!c[c.scheduler]) {
-      throw new Error(`App ${id} is of scheduler ${c.scheduler} but config.${c.scheduler} is not set`);
-    }
-  });
+  // Object.entries(config).forEach(([id, c]) => {
+  //   if (!c[c.scheduler]) {
+  //     throw new Error(`App ${id} is of scheduler ${c.scheduler} but config.${c.scheduler} is not set`);
+  //   }
+  // });
 
   return config;
 };
