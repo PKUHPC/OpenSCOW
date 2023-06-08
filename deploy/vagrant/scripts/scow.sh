90520ee39e21a8e180fc4d5faf8339895de2b0f2
Huangjun <huangjun0210-17@126.com>
2023-06-08 10:35:28 +0800

    修复export-jobs报错

--- deploy/vagrant/scripts/scow.sh
+++ deploy/vagrant/scripts/scow.sh
@@ -2,6 +2,10 @@
 \cp -r /vagrant/scow/scow-deployment/* /root/scow/scow-deployment/
 \cp -r /vagrant/scow/export-jobs/* /root/scow/export-jobs/
 
+# 修复export-jobs报错
+pip3 install -U pip setuptools
+pip3 install cryptography -i https://pypi.douban.com/simple
+
 echo "*/10 * * * * root python3 /root/scow/export-jobs/main.py >> /root/scow/export-jobs/job_export.log 2>&1" >> /etc/crontab
 
 rm -rf /root/.docker/config.json
