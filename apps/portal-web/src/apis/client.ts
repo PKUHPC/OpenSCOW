864da70fb49af33620fb53ec75ce120943b36527
picca Sun <piccasun@gmail.com>
2023-06-09 09:37:25 +0000

    apiClient fix

--- apps/portal-web/src/apis/client.ts
+++ apps/portal-web/src/apis/client.ts
@@ -14,6 +14,6 @@ import { createApiClient } from "@ddadaal/next-typed-api-routes-runtime/lib/clie
 import { publicConfig } from "src/utils/config";
 
 export const apiClient = createApiClient({
-  baseUrl: (typeof window === "undefined") ? `http://localhost:${process.env.PORT ?? 3000}` : ""
+  baseUrl: ((typeof window === "undefined") ? `http://localhost:${process.env.PORT ?? 3000}` : "")
    + (publicConfig.BASE_PATH === "/" ? "" : publicConfig.BASE_PATH),
 });
