6e4bf5e8301f2ec9da12c4910150358d33e73088
ZihanChen821 <827625357@qq.com>
2023-06-15 06:13:05 +0000

    fix test

--- apps/portal-server/tests/utils/parseIp.test.ts
+++ apps/portal-server/tests/utils/parseIp.test.ts
@@ -12,14 +12,12 @@
 
 import { parseIp } from "src/utils/proxy";
 
-const stdout = `
-PING cn01 (172.58.1.111) 56(84) bytes of data.
+const stdout = `PING cn01 (172.58.1.111) 56(84) bytes of data.
 64 bytes from cn01 (172.58.1.111): icmp_seq=1 ttl=64 time=0.154 ms
 
 --- cn01 ping statistics ---
 1 packets transmitted, 1 received, 0% packet loss, time 0ms
-rtt min/avg/max/mdev = 0.154/0.154/0.154/0.000 ms
-`;
+rtt min/avg/max/mdev = 0.154/0.154/0.154/0.000 ms`;
 
 it("parses IP address from stdout", () => {
   const ip = parseIp(stdout);
