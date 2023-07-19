a978233feb2d0124aee470a90183c1f00e7da241
Chen Junda <ddadaal@outlook.com>
2023-07-19 09:17:09 +0800

    fix(web): 修复antd主题未能应用到整体页面 (#745)
    按现在的前端的架构，页面主要由实现在libs/web下的总体布局以及实现在portal-web或者mis-web里的业务组件组成。portal-web和mis-web将会从libs/web导入UI组件并渲染在页面上。特别的，portal-web和mis-web将会在libs/web下导入一个AntdConfigProvider组件，这个组件引入了`antd`的ConfigProvider，用于配置UI的主题。
    
    
    在上一次依赖项升级后，libs/web中的AntdConfigProvider组件配置的主题不会影响portal-web和mis-web中的UI。
    
    
    ![img_v2_49fda615-76cc-4e74-92a5-f80507ba5d6g](https://github.com/PKUHPC/SCOW/assets/8363856/479f5981-9a7b-4b40-a3a5-020ab45a892e)
    
    
    此问题具体原因不明，但有可能是因为pnpm的依赖解析算法导致导入了多个antd或者react实例。已确认并非libs/web项目的peerDependencies导致的。本PR同时修改了libs/web项目的peerDependencies，在其中加入了react,
    react-dom等其他依赖项，但并未解决此问题。
    
    
    此PR提出了临时的解决方案，即在portal-web和mis-web中复制粘贴一份AntdConfigProvider组件。这个组件直接从antd导入`ConfigProvider`来配置portal-web和mis-web中的UI的主题，并同时使用了libs/web中的`AntdConfigProvider`以配置libs/web下的UI。

---
+++ apps/portal-web/src/layouts/AntdConfigProvider.tsx
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2022 Peking University and Peking University Institute for Computing and Digital Economy
+ * SCOW is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+import "dayjs/locale/zh-cn";
+
+import { AntdConfigProvider as LibAntdConfigProvider } from "@scow/lib-web/build/layouts/AntdConfigProvider";
+import { useDarkMode } from "@scow/lib-web/build/layouts/darkMode";
+import { App, ConfigProvider, theme } from "antd";
+import zhCNlocale from "antd/locale/zh_CN";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+type Props = React.PropsWithChildren<{
+  color: string;
+}>;
+
+const StyledComponentsThemeProvider: React.FC<Props> = ({ children }) => {
+  const { token } = theme.useToken();
+
+  return (
+    <ThemeProvider theme={{ token }}>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export const AntdConfigProvider: React.FC<Props> = ({ children, color }) => {
+
+  const { dark } = useDarkMode();
+
+  return (
+    <LibAntdConfigProvider color={color}>
+      <ConfigProvider
+        locale={zhCNlocale}
+        theme={{ token: { colorPrimary: color, colorInfo: color }, algorithm: dark ? theme.darkAlgorithm : undefined }}
+      >
+        <StyledComponentsThemeProvider color={color}>
+          <App>
+            {children}
+          </App>
+        </StyledComponentsThemeProvider>
+      </ConfigProvider>
+    </LibAntdConfigProvider>
+  );
+};
