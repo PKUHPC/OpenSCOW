271486cfa97d5b93ac0a91cb7b9ee0b4060c0a44
Huangjun <huangjun0210-17@126.com>
2023-07-26 14:08:57 +0800

    删除export-jobs和源作业数据库

--- deploy/vagrant/scripts/scow.sh
+++ deploy/vagrant/scripts/scow.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 \cp -r /vagrant/scow/scow-deployment/* /root/scow/scow-deployment/
-\cp -r /vagrant/scow/export-jobs/* /root/scow/export-jobs/
 
-# 修复export-jobs报错
-pip3 install -U pip setuptools
-pip3 install cryptography -i https://pypi.douban.com/simple
-
-echo "*/10 * * * * root python3 /root/scow/export-jobs/main.py >> /root/scow/export-jobs/job_export.log 2>&1" >> /etc/crontab
+docker stop sourcedb && docker rm  sourcedb
 
 rm -rf /root/.docker/config.json
 
