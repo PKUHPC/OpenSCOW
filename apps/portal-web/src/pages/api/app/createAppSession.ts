0eb733162fb0ab8122b0377ed1f6173c4b983661
何童崇 <792998983@qq.com>
2023-08-10 03:31:01 +0000

    fix

--- apps/portal-web/src/pages/api/app/createAppSession.ts
+++ apps/portal-web/src/pages/api/app/createAppSession.ts
@@ -114,7 +114,7 @@ export default /* #__PURE__*/route(CreateAppSessionSchema, async (req, res) => {
         return e;
       }
     } else {
-      return { 409: { code: "SBATCH_FAILED" as const, message: ex.details } };
+      throw e;
     }
   });
 });
