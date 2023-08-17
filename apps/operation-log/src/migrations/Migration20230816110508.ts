c53983e1e8263667b1bc372ce90d1ce165508d31
ZihanChen821 <827625357@qq.com>
2023-08-17 01:11:29 +0000

    fix: migration sql

--- apps/operation-log/src/migrations/Migration20230816110508.ts
+++ apps/operation-log/src/migrations/Migration20230816110508.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20230816110508 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('alter table `operation_log` modify `operation_time` DATETIME(6) not null default current_timestamp(0), modify `operation_result` enum(\'UNKNOWN\', \'SUCCESS\', \'FAIL\') not null comment \'UNKNOWN, SUCCESS, FAIL\';');
+    this.addSql('alter table `operation_log` modify `operation_time` DATETIME(6) not null default current_timestamp(6), modify `operation_result` enum(\'UNKNOWN\', \'SUCCESS\', \'FAIL\') not null comment \'UNKNOWN, SUCCESS, FAIL\';');
   }
 
   async down(): Promise<void> {
