e8d4b17eadb483a23911003a174c853708759479
picca Sun <piccasun@gmail.com>
2023-09-15 05:38:41 +0000

    fix dockerfile

---
+++ libs/web/src/utils/libWebI18nTexts.ts
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2022 Peking University and Peking University Institute for Computing and Digital Economy
+ * SCOW is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+export const libWebZhCn = {
+  comp: {
+    clusterSelector: {
+      placeholder: "请选择集群",
+    },
+  },
+  layouts: {
+    smallScreenMenu: {
+      welcome: "欢迎",
+      logout: "退出登录",
+    },
+    userIndicator: {
+      userName: "用户姓名",
+      userId: "用户ID",
+      personalInfo: "个人信息",
+      logout: "退出登录",
+      login: "登录",
+    },
+    darkMode: {
+      system: "跟随系统",
+      light: "亮色",
+      dark: "暗色",
+    },
+    defaultClusterSelector: {
+      title: "需要选择集群的功能将会默认选择默认集群",
+      label: "选择默认集群",
+    },
+  },
+  utils: {
+    dateTime: {
+      today: "今天",
+      tWeek: "本周",
+      tMonth: "本月",
+      tYear: "今年",
+      threeMonths: "3个月",
+      sixMonths: "6个月",
+      oneYear: "一年",
+    },
+    form: {
+      confirmPasswordForm: {
+        message: "请确认密码",
+        notEqualError: "两次密码输入不一致，请重新输入",
+        emailError: "邮箱格式不正确，请重新输入",
+      },
+    },
+    refreshToken: {
+      refreshButton: "刷新",
+    },
+  },
+};
+
+export const libWebEn = {
+  comp: {
+    clusterSelector: {
+      placeholder: "请选择集群",
+    },
+  },
+  layouts: {
+    smallScreenMenu: {
+      welcome: "欢迎",
+      logout: "退出登录",
+    },
+    userIndicator: {
+      userName: "用户姓名",
+      userId: "用户ID",
+      personalInfo: "个人信息",
+      logout: "退出登录",
+      login: "登录",
+    },
+    darkMode: {
+      system: "跟随系统",
+      light: "亮色",
+      dark: "暗色",
+    },
+    defaultClusterSelector: {
+      title: "需要选择集群的功能将会默认选择默认集群",
+      label: "选择默认集群",
+    },
+  },
+  utils: {
+    dateTime: {
+      today: "今天",
+      tWeek: "本周",
+      tMonth: "本月",
+      tYear: "今年",
+      threeMonths: "3个月",
+      sixMonths: "6个月",
+      oneYear: "一年",
+    },
+    form: {
+      confirmPasswordForm: {
+        message: "请确认密码",
+        notEqualError: "两次密码输入不一致，请重新输入",
+        emailError: "邮箱格式不正确，请重新输入",
+      },
+    },
+    refreshToken: {
+      refreshButton: "刷新",
+    },
+  },
+};
+
