93ec329c0fb0ff733a871ff7aa95040f1e657a10
Miracle575 <longsijie@icode.pku.edu.cn>
2023-11-16 05:02:05 +0000

    更新 test

--- apps/cli/tests/compose.test.ts
+++ apps/cli/tests/compose.test.ts
@@ -61,7 +61,9 @@ describe("sets custom auth environment", () => {
     const configPath = await createInstallYaml({
       auth: {
         custom: {
-          image: "",
+          image: {
+            imageName: "",
+          },
           environment: {
             "CUSTOM_AUTH_KEY": "CUSTOM_AUTH_VALUE",
           },
@@ -79,7 +81,9 @@ describe("sets custom auth environment", () => {
     const configPath = await createInstallYaml({
       auth: {
         custom: {
-          image: "",
+          image: {
+            imageName: "",
+          },
           environment: [
             "CUSTOM_AUTH_KEY=CUSTOM_AUTH_VALUE",
           ],
