4a63af23fc5d4890561b593ed2cfe777b29787c3
huangjun0210 <huangjun0210-17@126.com>
2023-12-09 10:58:57 +0800

    fix(deploy): vagrant部署方式优化，支持自定义scow和适配器版本 (#1028)

--- deploy/vagrant/scripts/scow.sh
+++ deploy/vagrant/scripts/scow.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+rm -rf /root/scow/scow-deployment/config/apps/app1.yaml
 \cp -r /vagrant/scow/scow-deployment/* /root/scow/scow-deployment/
 
 docker stop sourcedb && docker rm  sourcedb
 
+chmod +x /root/scow/scow-deployment/cli
 rm -rf /root/scow/export-jobs/
 
 rm -rf /root/.docker/config.json
