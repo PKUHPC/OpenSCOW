b9fbec6c3e9fa9fab8331b856675d7e736efd79a
OYX-1 <13121812323@163.com>
2024-03-04 08:57:25 +0000

    fix:仪表盘cpu和gpu利用率百分比显示错误

--- apps/portal-web/src/pages/dashboard.tsx
+++ apps/portal-web/src/pages/dashboard.tsx
@@ -90,9 +90,9 @@ export const DashboardPage: NextPage<Props> = requireAuth(() => true)(() => {
           cluster.clusterInfo.partitions.map((x) => ({
             clusterName: cluster.clusterInfo.clusterName,
             ...x,
-            cpuUsage:(x.runningCpuCount / x.cpuCoreCount).toFixed(2),
+            cpuUsage:((x.runningCpuCount / x.cpuCoreCount) * 100).toFixed(2),
             // 有些分区没有gpu就为空，前端显示'-'
-            ...x.gpuCoreCount ? { gpuUsage:(x.runningGpuCount / x.gpuCoreCount).toFixed(2) } : {},
+            ...x.gpuCoreCount ? { gpuUsage:((x.runningGpuCount / x.gpuCoreCount) * 100).toFixed(2) } : {},
           })),
         );
 
