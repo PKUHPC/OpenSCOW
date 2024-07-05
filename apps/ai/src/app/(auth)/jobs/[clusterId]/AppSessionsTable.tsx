bdcf43a09291f804fc36e4de8a73232a9183d5aa
OYX-1 <13121812323@163.com>
2024-07-05 02:33:10 +0000

    fix: ai获取作业默认排序

--- apps/ai/src/app/(auth)/jobs/[clusterId]/AppSessionsTable.tsx
+++ apps/ai/src/app/(auth)/jobs/[clusterId]/AppSessionsTable.tsx
@@ -87,6 +87,7 @@ export const AppSessionsTable: React.FC<Props> = ({ cluster, status }) => {
       title: "作业ID",
       dataIndex: "jobId",
       width: "8%",
+      defaultSortOrder: 'descend',
       sorter: (a, b) => compareNumber(a.jobId, b.jobId),
     },
     {
@@ -258,6 +259,7 @@ export const AppSessionsTable: React.FC<Props> = ({ cluster, status }) => {
 
   const filteredData = useMemo(() => {
     if (!data) { return []; }
+
     return data.sessions.filter((x) => {
       if (query.appJobName) {
         return x.sessionId.toLowerCase().includes(query.appJobName!.toLowerCase());
@@ -267,7 +269,7 @@ export const AppSessionsTable: React.FC<Props> = ({ cluster, status }) => {
       ...x,
       remainingTime: x.state === "RUNNING" ? calculateAppRemainingTime(x.runningTime, x.timeLimit) :
         x.state === "PENDING" ? "" : x.timeLimit,
-    }));
+    }))
   }, [data, query]);
 
   return (
