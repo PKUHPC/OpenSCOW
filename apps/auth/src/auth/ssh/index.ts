9e67d1efe1735d53212d27aff99217f9bb203af7
link89 <xuweihong.cn@gmail.com>
2024-09-20 19:59:08 +0800

    fix(auth): provide a default value for full name (#1428)
    close https://github.com/PKUHPC/SCOW/issues/1426

--- apps/auth/src/auth/ssh/index.ts
+++ apps/auth/src/auth/ssh/index.ts
@@ -59,14 +59,14 @@ export const createSshAuthProvider = (f: FastifyInstance) => {
       return await sshConnect(loginNode, "root", rootKeyPair, req.log, async (ssh) => {
 
         const resp = await loggedExec(ssh, req.log, false, "getent", ["passwd", identityId]);
-
         if (resp.code !== 0) { return undefined; }
-
         // https://en.wikipedia.org/wiki/Gecos_field
         // ddadaal:x:1000:1000::/home/ddadaal:/bin/zsh
-        const gecosField = resp.stdout.split(":")[4];
-        const fullName = gecosField.split(",")[0];
+        const passwdItems = resp.stdout.split(":");
+        const gecosField = passwdItems[4];
 
+        // use username as default full name
+        const fullName = gecosField?.split(",")?.[0] || passwdItems[0];
         return {
           identityId,
           name: fullName,
