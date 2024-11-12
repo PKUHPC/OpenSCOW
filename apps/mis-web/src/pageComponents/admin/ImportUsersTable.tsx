71466e608d0128022e023d1d5eec024758350766
何童崇 <792998983@qq.com>
2024-11-12 09:30:57 +0800

    fix: 导入用户页面的将用户添加入白名单放置在导入按键附近 (#75)

--- apps/mis-web/src/pageComponents/admin/ImportUsersTable.tsx
+++ apps/mis-web/src/pageComponents/admin/ImportUsersTable.tsx
@@ -151,6 +151,11 @@ export const ImportUsersTable: React.FC = () => {
                 Router.push({ query: { cluster: value.id } });
               }}
             />
+
+            <Form.Item name="whitelist" valuePropName="checked">
+              <Checkbox>{t(p("addWhitelist"))}</Checkbox>
+            </Form.Item>
+
             <Button type="primary" htmlType="submit" loading={loading}>
               {t(pCommon("import"))}
             </Button>
@@ -233,9 +238,7 @@ export const ImportUsersTable: React.FC = () => {
             )}
           />
         </Table>
-        <Form.Item name="whitelist" valuePropName="checked">
-          <Checkbox>{t(p("addWhitelist"))}</Checkbox>
-        </Form.Item>
+
         <Drawer
           placement="right"
           onClose={() => setusersList(undefined)}
