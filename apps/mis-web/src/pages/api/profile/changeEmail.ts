83c0b68d988751b004365ae5427f3542c800f4c9
OYX-1 <74037789+OYX-1@users.noreply.github.com>
2026-01-28 15:00:24 +0800

    Update changeEmail.ts

--- apps/mis-web/src/pages/api/profile/changeEmail.ts
+++ apps/mis-web/src/pages/api/profile/changeEmail.ts
@@ -29,7 +29,6 @@ export const ChangeEmailSchema = typeboxRouteSchema({
   method: "PATCH",
 
   body: Type.Object({
-    userId:Type.String(),
     newEmail: Type.String(),
   }),
 
@@ -49,13 +48,14 @@ export const ChangeEmailSchema = typeboxRouteSchema({
 });
 
 export default /* #__PURE__*/typeboxRoute(ChangeEmailSchema, async (req, res) => {
-  const { userId, newEmail } = req.body;
-  const auth = authenticate((info) => userId === info.identityId);
+  const auth = authenticate(() => true);
 
   const info = await auth(req, res);
 
   if (!info) { return; }
-
+  
+  const { newEmail } = req.body;
+  
   const ldapCapabilities = await getCapabilities(runtimeConfig.AUTH_INTERNAL_URL);
   if (!ldapCapabilities.changeEmail) {
     return { 501: null };
@@ -64,7 +64,7 @@ export default /* #__PURE__*/typeboxRoute(ChangeEmailSchema, async (req, res) =>
   const client = getClient(UserServiceClient);
 
   return await asyncClientCall(client, "changeEmail", {
-    userId,
+    userId:info.identityId,
     newEmail,
   })
     .then(() => ({ 204: null }))
